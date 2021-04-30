11de81dd92aa126540940ddd9ddb64c2061ceec5
chdonovan <chrisdonovan311@gmail.com>
2021-04-30 12:04:43 -0500

    UV index added

--- assets/script.js
+++ assets/script.js
@@ -60,9 +60,24 @@ searchButton.click(function(){
             // add wind speed
             currentTemp.append("<p>" + "Wind Speed: " + response.wind.speed + "<p>");
 
-            
+            // Uv Index URL
+            var urlUV = `https://api.openweathermap.org/data/2.5/uvi?appid=b8ecb570e32c2e5042581abd004b71bb&lat=${response.coord.lat}&lon=${response.coord.lon}`;
 
-        })
+
+            // UV index
+            $.ajax({
+                url: urlUV,
+                method: "GET"
+            }).then(function(response){
+
+                var currentUV = currentTemp.append("<p>" + "UV Index: " + response.value + "<p>").addClass("card-text");
+                currentUV.addClass("UV");
+                currentTemp.append(currentUV);
+
+            });
+        
+        });
+        
     }
 
 
